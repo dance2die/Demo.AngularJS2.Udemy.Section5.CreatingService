3a200f7c95928fa8bbe747b36dd9762498a8d122
dance2die <dontbothermewithspam@gmail.com>
2016-05-25 07:10:19 -0400

    Angular2: Sharing an instance of a service.

--- dev/component2.component.ts
+++ dev/component2.component.ts
@@ -1,11 +1,19 @@
 import {Component} from 'angular2/core';
+import {LoggingService} from "./services/logging.service";
 
 @Component({
     selector: 'component-2',
     template: `
-    
+        <input type="text" #message>
+        <button (click)="onLog(message.value)">Send</button>
+
     `
 })
 export class Component2Component {
-    
+    constructor(private _loggingService: LoggingService){
+    }
+
+    onLog(message: string){
+        this._loggingService.log(message);
+    }
 }
--- dev/services/logging.service.ts
+++ dev/services/logging.service.ts
@@ -2,8 +2,11 @@ import {Injectable} from "angular2/core";
 
 @Injectable()
 export class LoggingService {
+    private _lastMessage = '';
+
     log(message: string){
-        console.log(message);
+        console.log("Current Message: " + message + ", Last Message: " + this._lastMessage);
+        this._lastMessage = message;
     }
 }
 
